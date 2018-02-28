32368c353a253afe3c5c25e9e48f05ee61cf60f1
Yauheni Kaliuta <y.kaliuta@gmail.com>
2018-03-01 01:12:37 +0200

    tests: queue: add three_elements_are_the_same_after_read
    Check if the data are the same and in the same order.
    
    The test also checks, that empty/full predicates are correct for
    the 3-element queue.
    
    Signed-off-by: Yauheni Kaliuta <y.kaliuta@gmail.com>

--- tests/queue-test.c
+++ tests/queue-test.c
@@ -76,6 +76,37 @@ Ensure(one_element_size_full_after_one_element)
 	queue_destroy(q);
 }
 
+Ensure(three_elements_are_the_same_after_read)
+{
+	struct queue *q;
+	int el1 = 1;
+	int el2 = 2;
+	int el3 = 3;
+	int el = 0;
+	bool rc;
+
+	q = queue_new(3, sizeof(el));
+	assert_that(q, is_not_null);
+
+	queue_push(q, &el1);
+	queue_push(q, &el2);
+	queue_push(q, &el3);
+	rc = queue_is_full(q);
+	assert_that(rc, is_true);
+
+	queue_pop(q, &el);
+	assert_that(el, is_equal_to(1));
+	queue_pop(q, &el);
+	assert_that(el, is_equal_to(2));
+	queue_pop(q, &el);
+	assert_that(el, is_equal_to(3));
+
+	rc = queue_is_empty(q);
+	assert_that(rc, is_true);
+
+	queue_destroy(q);
+}
+
 TestSuite *my_create_test_suite(void)
 {
 	TestSuite *suite = create_named_test_suite(NAME);
@@ -86,6 +117,7 @@ TestSuite *my_create_test_suite(void)
 	add_test(suite, one_element_size_creates_nonfull_queue);
 	add_test(suite, one_element_size_accepts_one_element);
 	add_test(suite, one_element_size_full_after_one_element);
+	add_test(suite, three_elements_are_the_same_after_read);
 
 	set_teardown(suite, teardown);
 
